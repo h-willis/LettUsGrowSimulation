36cad45fc50c91c6e3f1aa28b735fa6c86f906ec
h-willis <harry.willis@btconnect.com>
2021-03-20 08:30:06 +0000

    corrected bed col / row

--- bed.py
+++ bed.py
@@ -3,22 +3,28 @@
 # where they can draw or dump to
 
 class Bed:
-    def __init__(self, row, column):
-        self.row = row
+    def __init__(self, column, row):
         self.col = column
+        self.row = row
         self.target = None
         self.target_min = -1
         self.target_max = -1
         self.water_level = -1
         self.valve_status = None
         self.capacity = -1
+        # self.target = 'Fill'
+        # self.target_min = 20
+        # self.target_max = 30
+        # self.water_level = 0
+        # self.valve_status = 'close'
+        # self.capacity = 100
         # whether or not the bed is at the right level
         self.happy = False
 
     def __str__(self):
         # return(f"{self.row}{self.col}, Level:{self.current_level}, Targ:{self.target}, Valve:{self.valve_status}, Happy:{self.happy}")
         return "{}{}: Level:{}, Targ:{}, FillMin:{}, Valve:{}, Happy:{}" \
-        .format(self.row, self.col, self.water_level, self.target, self.target_min, self.valve_status, self.isHappy())
+        .format(self.col, self.row, self.water_level, self.target, self.target_min, self.valve_status, self.isHappy())
 
     def setValve(self, state, client, user):
         # if valve was asked to open and is currently closed
