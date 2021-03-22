6ab7e5acbaf022b86c7f54723c1eb8eec8850070
h-willis <harry.willis@btconnect.com>
2021-03-22 17:31:09 +0000

    cleanup and adding docstrings to bed.py

--- bed.py
+++ bed.py
@@ -1,8 +1,9 @@
-
-# create object for each bed and try to develop some logic for the beds to know
-# where they can draw or dump to
+# stores relevant information for each bed
 
 class Bed:
+    """
+    Stores relevant metadata for each bed as well as some handy status funcs
+    """
     def __init__(self, column, row):
         self.col = column
         self.row = row
@@ -12,21 +13,27 @@ class Bed:
         self.water_level = -1
         self.valve_status = None
         self.capacity = -1
-        # self.target = 'Fill'
-        # self.target_min = 20
-        # self.target_max = 30
-        # self.water_level = 0
-        # self.valve_status = 'close'
-        # self.capacity = 100
         # whether or not the bed is at the right level
         self.happy = False
 
+
     def __str__(self):
-        # return(f"{self.row}{self.col}, Level:{self.current_level}, Targ:{self.target}, Valve:{self.valve_status}, Happy:{self.happy}")
-        return "{}{}: Level:{}, Targ:{}, FillMin:{}, Valve:{}, Happy:{}" \
-        .format(self.col, self.row, self.water_level, self.target, self.target_min, self.valve_status, self.isHappy())
+        """
+        Prints some useful information to give a quick display of a beds status
+        """
+        return "{}{}: Level:{}, Targ:{}, Valve:{}, Happy:{}" \
+        .format(self.col, self.row, self.water_level, self.target, self.valve_status, self.isHappy())
+
 
     def setValve(self, state, client, user):
+        """
+        Checks current valve state and sends MQTT message to change to requested
+        state if required
+        Parameters:
+        state: Requested state, 'open' or 'close'
+        client: MQTT client to send message through
+        user: User deets for MQTT message
+        """
         # if valve was asked to open and is currently closed
         if state == 'open' and self.valve_status == 'close':
             client.publish(f"{user}/bed-{self.col}{self.row}/valve/set", 'open')
@@ -36,17 +43,32 @@ class Bed:
             client.publish(f"{user}/bed-{self.col}{self.row}/valve/set", 'close')
             self.valve_status = 'close'
 
-    # returns if bed has correct level
+
     def isHappy(self):
+        """
+        Returns T/F depending if bed is at correct level for it's target
+        """
         return (self.target == 'Fill' and (self.water_level >= self.target_min and self.water_level < self.target_max)) \
                or (self.target == 'Empty' and self.water_level == 0)
 
+
     def isOpen(self):
+        """
+        Returns T/F if valve is open or not
+        """
         return self.valve_status == 'open'
 
+
     def needsFilling(self):
+        """
+        Returns T/F if more water is required in this bed
+        """
         return self.target == 'Fill' and self.water_level < self.target_min
 
+
     def needsDraining(self):
-        return (self.target == 'Empty' and self.water_level > 0) \
-                or (self.target == 'Fill' and self.water_level > self.target_max)
+        """
+        Returns T/F if water needs removing from bed. Takes into account overfilling
+        """
+        return (self.target == 'Empty' and self.water_level > 0) or \
+               (self.target == 'Fill' and self.water_level > self.target_max)
